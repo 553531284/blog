bedb05a4dcef37b543989db4480584ca2cdf8ffa
dengzhou <553531284@qq.com>
2019-04-16 23:48:35 +0800

    Site updated: 2019-04-16 23:48:32

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
     if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
     else $('#footer').removeAttr('style position');
+    $('#footer').slideDown();
   }
   updateFooterPosition();
   $(window).on('resize scroll', updateFooterPosition);
